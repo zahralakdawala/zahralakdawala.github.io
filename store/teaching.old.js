f0fea69c30afec97a9af96c131d231bc1a614179
Moiz Sohail <moizsohail@live.co.uk>
2021-01-07 03:18:00 +0500

    diversify

---
+++ store/teaching.old.js
@@ -0,0 +1,155 @@
+export const state = () => ({
+  data: [
+    {
+      head: 'Math',
+      link: 'math310',
+      title: 'Ordinary Differential Equations',
+      fullTitle: 'Ordinary Differential Equations',
+      text: 'Blah',
+      modules: [
+        {
+          title: 'Course Outline:',
+          text: '',
+          content: [
+            {
+              type: 'PDF',
+              link: '/Teaching/Math310/MATH310ODE_Lakdawala.pdf',
+              title: 'Course Outline',
+            },
+          ],
+        },
+        {
+          title: 'Module 1:',
+          text: 'This module will review the basic concepts of Linear Algebra',
+          content: [
+            {
+              type: 'PDF',
+              link: '/Teaching/Math4418/Spring2021 - Class Schedule.pdf',
+              title: 'Lecture',
+            },
+            {
+              type: 'Video',
+              link: 'https://www.youtube.com/watch?v=xTsYYxf2Ut0',
+              title: 'Lecture',
+            },
+            {
+              type: 'Video',
+              link: 'https://www.youtube.com/watch?v=xTsYYxf2Ut0',
+              title: 'Optional',
+            },
+          ],
+        },
+        {
+          title: 'Module 2:',
+          text: 'This module will review the basic concepts of Linear Algebra',
+        },
+        {
+          title: 'Module 3:',
+          text: 'This module will review the basic concepts of Linear Algebra',
+        },
+        {
+          title: 'Module 4:',
+          text: 'This module will review the basic concepts of Linear Algebra',
+        },
+        {
+          title: 'Module 5:',
+          text: 'This module will review the basic concepts of Linear Algebra',
+        },
+        {
+          title: 'Module 6:',
+          text: 'This module will review the basic concepts of Linear Algebra',
+        },
+        {
+          title: 'Module 7:',
+          text: 'This module will review the basic concepts of Linear Algebra',
+        },
+        {
+          title: 'Module 8:',
+          text: 'This module will review the basic concepts of Linear Algebra',
+        },
+        {
+          title: 'Module 9:',
+          text: 'This module will review the basic concepts of Linear Algebra',
+        },
+      ],
+      img:
+        'https://hdwallpaperim.com/wp-content/uploads/2017/08/24/99791-mathematics-equation-black-derivative-748x421.jpg',
+    },
+    {
+      head: 'Math',
+      link: 'math449',
+      title: 'Quantitative Finance',
+      fullTitle: 'Quantitative Finance',
+      text: 'Blah',
+      modules: [
+        {
+          title: 'Course Outline:',
+          text: '',
+          content: [
+            {
+              type: 'PDF',
+              link: '/Teaching/Math310/MATH310ODE_Lakdawala.pdf',
+              title: 'Course Outline',
+            },
+          ],
+        },
+        {
+          title: 'Module 1:',
+          text: 'This module will review the basic concepts of Linear Algebra',
+          content: [
+            {
+              type: 'PDF',
+              link: '/Teaching/Math4418/Spring2021 - Class Schedule.pdf',
+              title: 'Lecture',
+            },
+            {
+              type: 'Video',
+              link: 'https://www.youtube.com/watch?v=xTsYYxf2Ut0',
+              title: 'Lecture',
+            },
+            {
+              type: 'Video',
+              link: 'https://www.youtube.com/watch?v=xTsYYxf2Ut0',
+              title: 'Optional',
+            },
+          ],
+        },
+        {
+          title: 'Module 2:',
+          text: 'This module will review the basic concepts of Linear Algebra',
+        },
+        {
+          title: 'Module 3:',
+          text: 'This module will review the basic concepts of Linear Algebra',
+        },
+        {
+          title: 'Module 4:',
+          text: 'This module will review the basic concepts of Linear Algebra',
+        },
+        {
+          title: 'Module 5:',
+          text: 'This module will review the basic concepts of Linear Algebra',
+        },
+        {
+          title: 'Module 6:',
+          text: 'This module will review the basic concepts of Linear Algebra',
+        },
+        {
+          title: 'Module 7:',
+          text: 'This module will review the basic concepts of Linear Algebra',
+        },
+        {
+          title: 'Module 8:',
+          text: 'This module will review the basic concepts of Linear Algebra',
+        },
+        {
+          title: 'Module 9:',
+          text: 'This module will review the basic concepts of Linear Algebra',
+        },
+      ],
+      img: 'https://wallpapercave.com/wp/wp2446263.jp',
+    },
+  ],
+})
+
+export const mutations = {}
